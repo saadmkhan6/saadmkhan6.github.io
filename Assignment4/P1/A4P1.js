0f53666a7622fdef5d2700a0f979369db52469e9
saadmkhan6 <saad.khan11@dcmail.ca>
2023-11-22 18:24:04 -0500

    Update A4P1.js

--- Assignment4/P1/A4P1.js
+++ Assignment4/P1/A4P1.js
@@ -13,16 +13,23 @@ const insertY = ['the soup kitchen', 'Disneyland', 'the White House'];
 const insertZ = ['spontaneously combusted', 'melted into a puddle on the sidewalk', 'turned into a slug and crawled away'];
 
 
-3. EVENT LISTENER AND PARTIAL FUNCTION DEFINITION
-
 randomize.addEventListener('click', result);
 
 function result() {
-
-  if(customName.value !== '') {
-    const name = customName.value;
-
-  }
+    let newStory = storyText;
+  
+    const xItem = randomValueFromArray(insertX);
+    const yItem = randomValueFromArray(insertY);
+    const zItem = randomValueFromArray(insertZ);
+  
+    newStory = newStory.replaceAll(':insertx:',xItem);
+    newStory = newStory.replaceAll(':inserty:',yItem);
+    newStory = newStory.replaceAll(':insertz:',zItem);
+  
+    if (customName.value !== '') {
+      const name = customName.value;
+      newStory = newStory.replaceAll('Bob', name);
+    }
 
   if(document.getElementById("uk").checked) {
     const weight = Math.round(300);
