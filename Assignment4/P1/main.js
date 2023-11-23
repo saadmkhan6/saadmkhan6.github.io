9a47e3e3271ca87543a7c915030b064033c35dc5
saadmkhan6 <saad.khan11@dcmail.ca>
2023-11-22 19:02:40 -0500

    finished p1

---
+++ Assignment4/P1/main.js
@@ -0,0 +1,45 @@
+const customName = document.getElementById('customname');
+const randomize = document.querySelector('.randomize');
+const story = document.querySelector('.story');
+
+function randomValueFromArray(array) {
+  const random = Math.floor(Math.random() * array.length);
+  return array[random];
+}
+
+const storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
+const insertX = ['Willy the Goblin', 'Big Daddy', 'Father Christmas'];
+const insertY = ['the soup kitchen', 'Disneyland', 'the White House'];
+const insertZ = ['spontaneously combusted', 'melted into a puddle on the sidewalk', 'turned into a slug and crawled away'];
+
+randomize.addEventListener('click', result);
+
+function result() {
+  let newStory = storyText;
+  const xItem = randomValueFromArray(insertX);
+  const yItem = randomValueFromArray(insertY);
+  const zItem = randomValueFromArray(insertZ);
+
+  newStory = newStory.replaceAll(':insertx:', xItem);
+  newStory = newStory.replaceAll(':inserty:', yItem);
+  newStory = newStory.replaceAll(':insertz:', zItem);
+
+  if (customName.value !== '') {
+    const name = customName.value;
+    newStory = newStory.replaceAll('Bob', name);
+  }
+
+  const isUKChecked = document.getElementById('uk').checked;
+  if (isUKChecked) {
+    const weightPounds = 300;
+    const weightStone = Math.round(weightPounds * 0.0714286);
+    const temperatureFahrenheit = 94;
+    const temperatureCelsius = Math.round((temperatureFahrenheit - 32) * 5 / 9);
+
+    newStory = newStory.replaceAll('94 fahrenheit', temperatureCelsius + ' celsius');
+    newStory = newStory.replaceAll('300 pounds', weightStone + ' stone');
+  }
+
+  story.textContent = newStory;
+  story.style.visibility = 'visible';
+}
